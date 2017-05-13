60ee64631f0883caed19b74f7c3c46678a6ae5ee
narmour <n.armour0@gmail.com>
2017-05-13 09:50:50 -0700

    started nn

---
+++ nn.py
@@ -0,0 +1,77 @@
+import numpy as np
+import struct
+from matplotlib import pyplot
+import matplotlib as mpl
+import random
+
+
+def getData():
+    #get traini 
+    with open('./train-labels.idx1-ubyte','rb') as fl:
+        magic,num = struct.unpack(">II",fl.read(8))
+        label = np.fromfile(fl,dtype=np.int8)
+    with open('./train-images.idx3-ubyte','rb') as fi:
+        magic, num, rows, cols = struct.unpack(">IIII",fi.read(16))
+        image = np.fromfile(fi,dtype=np.uint8).reshape(len(label),rows,cols)
+    get_image = lambda idx: (label[idx],image[idx])
+    ret = []
+    print("label: ",label)
+    for i in range(len(label)):
+        ret.append(get_image(i))
+    return ret
+
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show() 
+
+
+def extractFeatures(data):
+    print
+    ret = []
+    for image in data:
+        subgrid_values = []
+        #print(image[1])
+        #get all 7x7 subgrids in image[1]
+        for s in range(16):
+            values = []#one subgrid
+            for i in range(7):
+                for j in range(7):
+                    row = i + (4 * (s/4))
+                    col = j + (7 * (s % 4))
+                    #print("i: ",row," j: ",col)
+                    values.append(image[1][row][col])
+            print("subrgrid: ",s)
+            print(values,"\n\n")
+
+            
+
+        
+
+
+
+
+
+
+
+
+            
+
+
+
+def main():
+    data = getData()
+    print(len(data))
+    # 28 by 28 pixel grid
+    # each feature is 7x7 subgrid
+    training_data = extractFeatures(data[:1])
+    show(data[0][1])
+
+main()
